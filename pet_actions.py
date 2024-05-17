ae37786984523dff07d015be3784709af6db2492
Laurent Sun <LaPoudreRouge@gmail.com>
2024-05-18 01:54:38 +0200

    Balance change and removal of some print. Day pet hunger from -5 to -3. Day pet boredom from -3 to -2. Night pet hunger from +1 to +5. Night pet boredom from +1 to +5. Do not die when exhaustion reach 0 but just can't play. Put the print in play and feed into comments.

--- pet_actions.py
+++ pet_actions.py
@@ -7,7 +7,7 @@ def check_status():
 	check if any pet variable got to 0 or less
 	"""
 	for pet in v.pets:
-		if pet["health"] <= 0 or pet["exhaustion"] <= 0:
+		if pet["health"] <= 0:
 			v.game_state_change("game over")
 		if pet["boredom"] <= 0:
 			pet["bored"] = True
@@ -28,17 +28,17 @@ def status_update():
 			if pet["health"] < 200:
 				pet["health"] += 1
 			if pet["boredom"] < 200:
-				pet["boredom"] += 1
+				pet["boredom"] = min(pet["boredom"] + 5, 200)
 			if pet["exhaustion"] < 200:
-				pet["exhaustion"] += 1
+				pet["exhaustion"] = min(pet["exhaustion"] + 5, 200)
 
 		else:  # Awake
 			if pet["hunger"] > 0:
-				pet["hunger"] -= 5
+				pet["hunger"] -= 3
 			else:
 				pet["health"] -= 5
 			if pet["boredom"] > 0:
-				pet["boredom"] -= 3
+				pet["boredom"] -= 2
 			if any_pet_bored:
 				pet["health"] -= 5
 
@@ -70,7 +70,6 @@ def status_update_timer():
 		status_update()
 		day_night_cycle()
 		v.survival_time += 1
-		print(v.survival_time_separator(v.survival_time))
 
 
 def pet_feed():
@@ -82,10 +81,10 @@ def pet_feed():
 		if v.cookie_count > 0:
 			v.pets[v.current_pet]["hunger"] = min(v.pets[v.current_pet]["hunger"] + 50, 200)
 			v.cookie_count -= 1
-		else:
-			print("no more cookies")
-	else:
-		print("pet sleeping")
+	#	else:
+	#		print("no more cookies")
+	#else:
+	#	print("pet sleeping")
 
 
 def pet_play():
@@ -93,11 +92,11 @@ def pet_play():
 	play with the pet
 	add 50 to his boredom
 	"""
-	if v.day_state:
+	if v.day_state and (v.pets[v.current_pet]["exhaustion"] > 0):
 		v.pets[v.current_pet]["boredom"] = min(v.pets[v.current_pet]["boredom"] + 50, 200)
 		v.pets[v.current_pet]["exhaustion"] = max(v.pets[v.current_pet]["exhaustion"] - 50, 0)
-	else:
-		print("pet sleeping")
+	#else:
+	#	print("pet sleeping")
 
 
 def pet_reset():
